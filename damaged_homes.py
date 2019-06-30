b245448cf792d8ca126b94a244e6c380fbfce18a
geoJaSON <jasonjordan00@gmail.com>
2019-06-30 11:50:35 -0500

    Added points layers

--- damaged_homes.py
+++ damaged_homes.py
@@ -13,8 +13,11 @@ username = "jason.jordan_swg"
 password = "QAZw1s2x3"
 gis2 = GIS(gis_url,username,password)
 
-flooded_homes = gis2.content.get('bc7eb7d4138f4b828101719d81db6c33') # flooded homes layer
-flooded_homes
+damaged_homes = gis.content.get('b4e498c037fc4cc5b23bc1121a60e42a') # flooded homes layer
+damaged_homes
+
+destroyed_homes = gis.content.get('66cf5f24d1024f51a722488770d0079e')
+destroyed_homes
 
 
 vexcel = gis.content.get("60b3a9f1e1d04470ac71c52d681083ff")
