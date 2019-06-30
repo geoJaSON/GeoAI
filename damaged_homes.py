b550dd9fd6972de8a1e34795f6a02ad243f263ba
geoJaSON <jasonjordan00@gmail.com>
2019-06-30 13:47:53 -0500

    Combined Points

--- damaged_homes.py
+++ damaged_homes.py
@@ -13,12 +13,12 @@ username = "jason.jordan_swg"
 password = "QAZw1s2x3"
 gis2 = GIS(gis_url,username,password)
 
-damaged_homes = gis.content.get('b4e498c037fc4cc5b23bc1121a60e42a') # flooded homes layer
+damaged_homes = gis.content.get('8a07510d660d4543ac3f5e990176e729') # flooded homes layer
 damaged_homes
-
-destroyed_homes = gis.content.get('66cf5f24d1024f51a722488770d0079e')
-destroyed_homes
 
 
 vexcel = gis.content.get("60b3a9f1e1d04470ac71c52d681083ff")
 vexcel=vexcel.layers[0]
+
+chips = export_training_data(vexcel, damaged_homes, "PNG", {"x":448,"y":448}, {"x":224,"y":224},
+                             "PASCAL_VOC_rectangles", 75, "planetdemo")
